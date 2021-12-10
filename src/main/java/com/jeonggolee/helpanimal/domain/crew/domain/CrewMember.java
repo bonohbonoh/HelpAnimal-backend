14ac44f128153fa0008952087ad39a19b902404b
sh-mac <dltkdgns3435@naver.com>
2021-12-10 22:15:39 +0900

    HA-14 Backend : 크루, 크루맴버, 사용자 연관관계 매핑

---
+++ src/main/java/com/jeonggolee/helpanimal/domain/crew/domain/CrewMember.java
@@ -0,0 +1,39 @@
+package com.jeonggolee.helpanimal.domain.crew.domain;
+
+import com.jeonggolee.helpanimal.common.eneity.BaseTimeEntity;
+import com.jeonggolee.helpanimal.domain.crew.enums.CrewMemberRole;
+import com.jeonggolee.helpanimal.domain.user.entity.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@SequenceGenerator(
+        name = "crew_member_idx_generator",
+        sequenceName = "crew_member_idx"
+)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "crew_member")
+public class CrewMember extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "crew_member_idx_generator")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "crew_id", nullable = false, updatable = false)
+    private Crew crew;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CrewMemberRole role;
+
+
+}
