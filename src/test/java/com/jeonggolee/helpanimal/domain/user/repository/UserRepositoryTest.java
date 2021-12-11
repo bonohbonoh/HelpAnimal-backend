b8bd4390e8c3e745b832bff668537114d7196793
bonohbonoh <psmb2006@naver.com>
2021-12-11 18:46:08 +0900

    HA-12 Backend : 회원 Repository 생성 및 테스트코드 오류 수정

--- src/test/java/com/jeonggolee/helpanimal/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/com/jeonggolee/helpanimal/domain/user/repository/UserRepositoryTest.java
@@ -8,136 +8,126 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@Transactional
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
-    private Role guest = Role.GUEST;
+    private static final Role GUEST = Role.GUEST;
+    private static final String EMAIL = "test@test.com";
+    private static final String PASSWORD = "test password";
+    private static final String NICKNAME = "test nickname";
+    private static final String IMAGE = "test image";
+    private static final String NAME = "홍길동";
 
     @Test
-    @DisplayName("insert database")
-    @Transactional
+    @DisplayName("유저 생성 테스트")
     public void insertTest() {
-        IntStream.rangeClosed(0, 10).forEach(i -> {
-            //given
-            User user = User.builder()
-                    .email("test email" + i)
-                    .password("test password" + i)
-                    .name("n" + i)
-                    .nickName("test nick name" + i)
-                    .profileImage("basic image" + i)
-                    .role(guest)
-                    .build();
 
-            //when
-            User insertUser = userRepository.save(user);
+        //given
+        User user = User.builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .name(NAME)
+                .nickName(NICKNAME)
+                .profileImage(IMAGE)
+                .role(GUEST)
+                .build();
 
-            //then
-            assertThat(insertUser.getEmail()).isEqualTo("test email" + i);
-            assertThat(insertUser.getPassword()).isEqualTo("test password" + i);
-            assertThat(insertUser.getName()).isEqualTo("n" + i);
-            assertThat(insertUser.getNickName()).isEqualTo("test nick name" + i);
-            assertThat(insertUser.getProfileImage()).isEqualTo("basic image" + i);
-            assertThat(insertUser.getRole()).isEqualTo(guest);
-        });
+        //when
+        User saveUser = userRepository.save(user);
+
+        //then
+        assertThat(saveUser.getEmail()).isEqualTo(EMAIL);
+        assertThat(saveUser.getPassword()).isEqualTo(PASSWORD);
+        assertThat(saveUser.getName()).isEqualTo(NAME);
+        assertThat(saveUser.getNickName()).isEqualTo(NICKNAME);
+        assertThat(saveUser.getProfileImage()).isEqualTo(IMAGE);
+        assertThat(saveUser.getRole()).isEqualTo(GUEST);
     }
 
     @Test
-    @DisplayName("read database")
+    @DisplayName("유저 조회 테스트")
     public void readTest() {
-        IntStream.rangeClosed(0, 10).forEach(i -> {
-            //given
-            User user = User.builder()
-                    .email("test email" + i)
-                    .password("test password" + i)
-                    .name("n" + i)
-                    .nickName("test nick name" + i)
-                    .profileImage("basic image" + i)
-                    .role(guest)
-                    .build();
-            userRepository.save(user);
 
-            //when
-            List<User> UserList = userRepository.findAll();
+        //given
+        User user = User.builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .name(NAME)
+                .nickName(NICKNAME)
+                .profileImage(IMAGE)
+                .role(GUEST)
+                .build();
+        User saveUser = userRepository.save(user);
 
-            //then
-            assertThat(UserList.get(i).getEmail()).isEqualTo("test email" + i);
-            assertThat(UserList.get(i).getPassword()).isEqualTo("test password" + i);
-            assertThat(UserList.get(i).getName()).isEqualTo("n" + i);
-            assertThat(UserList.get(i).getNickName()).isEqualTo("test nick name" + i);
-            assertThat(UserList.get(i).getProfileImage()).isEqualTo("basic image" + i);
-            assertThat(UserList.get(i).getRole()).isEqualTo(guest);
-        });
+        //when
+        Long userId = saveUser.getUserId();
+
+        //then
+        User readUser = userRepository.getById(userId);
+        assertThat(readUser.getEmail()).isEqualTo(EMAIL);
+        assertThat(readUser.getPassword()).isEqualTo(PASSWORD);
+        assertThat(readUser.getName()).isEqualTo(NAME);
+        assertThat(readUser.getNickName()).isEqualTo(NICKNAME);
+        assertThat(readUser.getProfileImage()).isEqualTo(IMAGE);
+        assertThat(readUser.getRole()).isEqualTo(GUEST);
     }
 
     @Test
-    @DisplayName("update database")
+    @DisplayName("유저 수정 테스트")
     public void updateTest() {
+        //given
+        User user = User.builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .name(NAME)
+                .nickName(NICKNAME)
+                .profileImage(IMAGE)
+                .role(GUEST)
+                .build();
+        User saveUser = userRepository.save(user);
 
-        IntStream.rangeClosed(0, 10).forEach(i -> {
-            //given
-            User user = User.builder()
-                    .email("test email" + i)
-                    .password("test password" + i)
-                    .name("n" + i)
-                    .nickName("test nick name" + i)
-                    .profileImage("basic image" + i)
-                    .role(guest)
-                    .build();
-            userRepository.save(user);
-        });
+        //when
+        saveUser.updateNickName("update nickname");
+        saveUser.updatePassWord("update password");
+        saveUser.updateProfileImage("update image");
+        userRepository.save(saveUser);
 
-        LongStream.rangeClosed(1, 10).forEach(i -> {
-
-            //when
-            User userUpdateList = userRepository.findById(i).get();
-            userUpdateList.updateNickName("update nickname" + i);
-            userUpdateList.updateProfileImage("update image" + i);
-            userUpdateList.updatePassWord("update password" + i);
-            userRepository.save(userUpdateList);
-
-            //then
-            assertThat(userUpdateList.getNickName()).isEqualTo("update nickname" + i);
-            assertThat(userUpdateList.getProfileImage()).isEqualTo("update image" + i);
-            assertThat(userUpdateList.getPassword()).isEqualTo("update password" + i);
-        });
+        //then
+        User updateUser = userRepository.getById(user.getUserId());
+        assertThat(updateUser.getPassword()).isEqualTo("update password");
+        assertThat(updateUser.getNickName()).isEqualTo("update nickname");
+        assertThat(updateUser.getProfileImage()).isEqualTo("update image");
 
     }
 
     @Test
-    @DisplayName("delete database")
+    @DisplayName("유저 삭제 테스트")
     public void deleteTest() {
         //given
-        IntStream.rangeClosed(0, 10).forEach(i -> {
-            User user = User.builder()
-                    .email("test email" + i)
-                    .password("test password" + i)
-                    .name("n" + i)
-                    .nickName("test nick name" + i)
-                    .profileImage("basic image" + i)
-                    .role(guest)
-                    .build();
-            userRepository.save(user);
-        });
+        User user = User.builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .name(NAME)
+                .nickName(NICKNAME)
+                .profileImage(IMAGE)
+                .role(GUEST)
+                .build();
+        User saveUser = userRepository.save(user);
 
-        LongStream.rangeClosed(1, 10).forEach(i -> {
+        //when
+        Long deleteUserId = saveUser.getUserId();
+        userRepository.deleteById(deleteUserId);
 
-            //when
-            userRepository.deleteById(i);
-
-            //then
-            Optional<User> user = userRepository.findById(i);
-            assertThat(user.isPresent()).isFalse();
-
-        });
+        //then
+        Optional<User> deleteUser = userRepository.findById(deleteUserId);
+        assertThat(deleteUser.isPresent()).isFalse();
     }
 }
