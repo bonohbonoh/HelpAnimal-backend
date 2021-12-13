283847cd1c6eb49ae8cd2aeea7b9dee9e2888557
sh-mac <dltkdgns3435@naver.com>
2021-12-13 09:26:05 +0900

    HA-15 Backend : 크루, 크루맴버 레포지토리 @Repository 어노테이션 삭제

--- src/main/java/com/jeonggolee/helpanimal/domain/crew/repository/CrewRepository.java
+++ src/main/java/com/jeonggolee/helpanimal/domain/crew/repository/CrewRepository.java
@@ -4,6 +4,5 @@ import com.jeonggolee.helpanimal.domain.crew.domain.Crew;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CrewRepository extends JpaRepository<Crew, Long> {
 }
