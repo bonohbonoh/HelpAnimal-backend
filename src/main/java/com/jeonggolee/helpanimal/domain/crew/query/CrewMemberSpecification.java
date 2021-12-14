f8b86f32cbbec6ef50a936793a475a06b4757719
sh-mac <dltkdgns3435@naver.com>
2021-12-14 10:26:16 +0900

    HA-16 Backend : 크루맴버 레포지토리 JpaSpecification 추가 및 쿼리 추가

---
+++ src/main/java/com/jeonggolee/helpanimal/domain/crew/query/CrewMemberSpecification.java
@@ -0,0 +1,38 @@
+package com.jeonggolee.helpanimal.domain.crew.query;
+
+import com.jeonggolee.helpanimal.domain.crew.domain.Crew;
+import com.jeonggolee.helpanimal.domain.crew.domain.CrewMember;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CrewMemberSpecification {
+
+    //크루맴버 ID로 조회
+    public Specification<CrewMember> searchWithId(Long id){
+        return (root, query, builder) -> {
+            List<Predicate> predicateList = new ArrayList<>();
+
+            predicateList.add(this.withNotDeleted(root, builder));
+            predicateList.add(this.withId(id, root, builder));
+
+            return builder.and(predicateList.toArray(new  Predicate[0]));
+        };
+    }
+
+    //삭제되지 않은 조건 쿼리
+    private Predicate withNotDeleted(Root<CrewMember> root, CriteriaBuilder builder){
+        return builder.isNull(root.get("deletedAt"));
+    }
+
+    //크루맴버아이디 조건 쿼리
+    private Predicate withId(Long id, Root<CrewMember> root, CriteriaBuilder builder){
+        return builder.equal(root.get("id"), id);
+    }
+}
