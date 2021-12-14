fa6afde1e0578492d71166c4fb0a65d273d7fc5a
sh-mac <dltkdgns3435@naver.com>
2021-12-14 10:26:57 +0900

    HA-16 Backend : 크루 삭제 메서드 수정에 연관된 테스트 코드 수정 및 오타 수정

--- src/test/java/com/jeonggolee/helpanimal/domain/crew/repository/CrewRepositoryTest.java
+++ src/test/java/com/jeonggolee/helpanimal/domain/crew/repository/CrewRepositoryTest.java
@@ -8,11 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
-import static org.junit.jupiter.api.Assertions.assertThrows;//예외 테스트시 사용
 
 
 //@Transactional 사용시 테스트 하나 끝날때마다 롤백이 일어남. (테스트의 원자성)
@@ -29,7 +28,7 @@ class CrewRepositoryTest {
     @DisplayName("크루 생성")
     void saveCrewTest(){
         //given
-        Crew crew = Crew.builder().name("테스트크루").build();
+        Crew crew = Crew.builder().name("테스트크루").crewMemberList(new ArrayList<>()).build();
 
         //when
         Crew saveCrew = crewRepository.save(crew);
@@ -39,10 +38,10 @@ class CrewRepositoryTest {
     }
 
     @Test
-    @DisplayName("크루 조회")
+    @DisplayName("크루 아이디로 조회")
     void findCrewTest(){
         //given
-        Crew crew = Crew.builder().name("테스트크루").build();
+        Crew crew = Crew.builder().name("테스트크루").crewMemberList(new ArrayList<>()).build();
 
         //when
         Long saveId = crewRepository.save(crew).getId();
@@ -53,7 +52,7 @@ class CrewRepositoryTest {
     }
 
     @Test
-    @DisplayName("크루 조회 실패")
+    @DisplayName("크루 아이디로 조회 실패")
     void findCrewFailTest(){
         //given
 
@@ -69,7 +68,7 @@ class CrewRepositoryTest {
     @DisplayName("크루 이름으로 조회")
     void findCrewWithNameTest(){
         //given
-        Crew crew = Crew.builder().name("테스트크루").build();
+        Crew crew = Crew.builder().name("테스트크루").crewMemberList(new ArrayList<>()).build();
 
         //when
         crewRepository.save(crew);
@@ -83,7 +82,7 @@ class CrewRepositoryTest {
     @DisplayName("크루 이름으로 조회 실패")
     void findCrewWithNameFailTest(){
         //given
-        Crew crew = Crew.builder().name("테스트크루").build();
+        Crew crew = Crew.builder().name("테스트크루").crewMemberList(new ArrayList<>()).build();
 
         //when
         crewRepository.save(crew);
@@ -97,7 +96,7 @@ class CrewRepositoryTest {
     @DisplayName("크루 수정")
     void updateCrewTest(){
         //given
-        Crew crew = Crew.builder().name("테스트크루").build();
+        Crew crew = Crew.builder().name("테스트크루").crewMemberList(new ArrayList<>()).build();
 
         //when
         Long saveId = crewRepository.save(crew).getId();
@@ -116,7 +115,7 @@ class CrewRepositoryTest {
     @DisplayName("크루 삭제")
     void deleteCrewTest(){
         //given
-        Crew crew = Crew.builder().name("테스트크루").build();
+        Crew crew = Crew.builder().name("테스트크루").crewMemberList(new ArrayList<>()).build();
 
         //when
         Crew saveCrew = crewRepository.save(crew);
