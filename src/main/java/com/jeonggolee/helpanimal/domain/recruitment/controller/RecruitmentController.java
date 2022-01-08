686a1e63a34ea1763fd52e6aa3ac1ee612198286
Go Seung Hyeon <rhtld123@gmail.com>
2022-01-08 22:54:48 +0900

    HA-24 Backend : Controller, Test Code 추가

---
+++ src/main/java/com/jeonggolee/helpanimal/domain/recruitment/controller/RecruitmentController.java
@@ -0,0 +1,71 @@
+package com.jeonggolee.helpanimal.domain.recruitment.controller;
+
+import com.jeonggolee.helpanimal.domain.recruitment.dto.request.RecruitmentRegistDto;
+import com.jeonggolee.helpanimal.domain.recruitment.dto.request.RecruitmentUpdateDto;
+import com.jeonggolee.helpanimal.domain.recruitment.dto.response.RecruitmentDetailDto;
+import com.jeonggolee.helpanimal.domain.recruitment.dto.response.RecruitmentListDto;
+import com.jeonggolee.helpanimal.domain.recruitment.service.RecruitmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class RecruitmentController {
+    private final RecruitmentService recruitmentService;
+
+    /**
+     * 공고 등록
+     */
+    @PostMapping("/api/v1/recruitment")
+    public ResponseEntity<Long> registRecruitment(
+            @RequestBody @Valid RecruitmentRegistDto registDto) throws Exception {
+        return new ResponseEntity<Long>(recruitmentService.registRecruitment(registDto), HttpStatus.CREATED);
+    }
+
+    /**
+     * 공고 전체조회(페이징)
+     */
+    @GetMapping("/api/v1/recruitment")
+    public ResponseEntity<Page<RecruitmentListDto>> getAllRecruitmentList(
+            Pageable pageable) throws Exception {
+        return new ResponseEntity<Page<RecruitmentListDto>>(
+                recruitmentService.getRecriutmentListWithPaging(pageable), HttpStatus.OK);
+    }
+
+    /**
+     * 공고 수정
+     */
+    @PutMapping("/api/v1/recruitment/{id}")
+    public ResponseEntity updateRecruitment(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid RecruitmentUpdateDto updateDto) throws Exception {
+        recruitmentService.updateRecruitment(id,updateDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    /**
+     * 공고 상세조회
+     */
+    @GetMapping("/api/v1/recruitment/{id}")
+    public ResponseEntity<RecruitmentDetailDto> getRecruitmentDetail(
+            @PathVariable("id") Long id) throws Exception {
+        return new ResponseEntity<RecruitmentDetailDto>(
+                recruitmentService.getRecruitmentDetail(id),HttpStatus.OK);
+    }
+
+    /**
+     * 공고 삭제
+     */
+    @DeleteMapping("/api/v1/recruitment/{id}")
+    public ResponseEntity deleteRecruitment(@PathVariable("id") @Valid Long id) throws Exception {
+        recruitmentService.deleteRecruitment(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/com/jeonggolee/helpanimal/domain/recruitment/controller/RecruitmentControllerTest.java
@@ -0,0 +1,163 @@
+package com.jeonggolee.helpanimal.domain.recruitment.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jeonggolee.helpanimal.domain.recruitment.dto.request.RecruitmentRegistDto;
+import com.jeonggolee.helpanimal.domain.recruitment.dto.request.RecruitmentUpdateDto;
+import com.jeonggolee.helpanimal.domain.recruitment.entity.Animal;
+import com.jeonggolee.helpanimal.domain.recruitment.entity.Recruitment;
+import com.jeonggolee.helpanimal.domain.recruitment.enums.RecruitmentMethod;
+import com.jeonggolee.helpanimal.domain.recruitment.enums.RecruitmentType;
+import com.jeonggolee.helpanimal.domain.recruitment.repository.AnimalRepository;
+import com.jeonggolee.helpanimal.domain.recruitment.repository.RecruitmentRepository;
+import com.jeonggolee.helpanimal.domain.user.entity.User;
+import com.jeonggolee.helpanimal.domain.user.repository.UserRepository;
+import com.jeonggolee.helpanimal.domain.user.util.Role;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RecruitmentControllerTest {
+    private final String NAME = "테스트공고";
+    private final String EMAIL = "test1@naver.com";
+    private final String CONTENT = "본문";
+    private final String ANIMAL = "DOG";
+    private final int PARTICIPANT = 10;
+    private final RecruitmentMethod RECRUITMENT_METHOD = RecruitmentMethod.CHOICE;
+    private final RecruitmentType RECRUITMENT_TYPE = RecruitmentType.FREE;
+    private static Long RECRUITMENT_ID;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AnimalRepository animalRepository;
+
+    @Autowired
+    private RecruitmentRepository recruitmentRepository;
+
+    @BeforeAll
+    public void 유저_동물_등록() {
+        User user = initUser();
+        Animal animal = initAnimal(user);
+        initRecruitment(user, animal);
+    }
+
+    private User initUser() {
+        User user = User.builder()
+                .name("테스트")
+                .email(EMAIL)
+                .password("pass")
+                .nickname("닉네임")
+                .profileImage("image")
+                .role(Role.GUEST)
+                .build();
+        return userRepository.save(user);
+    }
+
+    private Animal initAnimal(User user) {
+        return animalRepository.save(Animal.builder()
+                .name(ANIMAL)
+                .user(user)
+                .build()
+        );
+    }
+
+    private void initRecruitment(User user, Animal animal) {
+        RECRUITMENT_ID = recruitmentRepository.save(
+                Recruitment.builder()
+                        .name(NAME)
+                        .user(user)
+                        .content(CONTENT)
+                        .animal(animal)
+                        .participant(PARTICIPANT)
+                        .recruitmentMethod(RECRUITMENT_METHOD)
+                        .recruitmentType(RECRUITMENT_TYPE)
+                        .build()).getId();
+    }
+
+    @Test
+    @WithMockUser(username = "test1@naver.com", roles = {"USER"})
+    public void 공고등록() throws Exception {
+        RecruitmentRegistDto dto = RecruitmentRegistDto.builder()
+                .name("공고등록_이름")
+                .email(EMAIL)
+                .content(CONTENT)
+                .animal(ANIMAL)
+                .participant(PARTICIPANT)
+                .recruitmentMethod(RECRUITMENT_METHOD)
+                .recruitmentType(RECRUITMENT_TYPE)
+                .build();
+
+        String content = objectMapper.writeValueAsString(dto);
+
+        mvc.perform(MockMvcRequestBuilders.post("/api/v1/recruitment")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content)).andExpect(status().isCreated());
+    }
+
+    @Test
+    @WithMockUser(username = "test1@naver.com", roles = {"USER"})
+    public void 공고페이징() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/recruitment?page=0&size=10&sort=id,desc")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "test1@naver.com", roles = {"USER"})
+    public void 공고수정() throws Exception {
+        RecruitmentUpdateDto dto = RecruitmentUpdateDto.builder()
+                .name("공고명_수정")
+                .content("공고내용_수정")
+                .participant(3)
+                .imageUrl("UPDATE_URL")
+                .build();
+
+        String content = objectMapper.writeValueAsString(dto);
+
+        mvc.perform(MockMvcRequestBuilders.put("/api/v1/recruitment/"+RECRUITMENT_ID)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "test1@naver.com", roles = {"USER"})
+    public void 공고_상세조회() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/recruitment/" + RECRUITMENT_ID)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "test1@naver.com", roles = {"USER"})
+    public void 공고삭제() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.delete("/api/v1/recruitment/" + RECRUITMENT_ID)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}
