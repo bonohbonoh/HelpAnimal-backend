f7b124a485feb00014c3793d6fc161b62f678de2
sh-mac <dltkdgns3435@naver.com>
2022-01-17 17:52:04 +0900

    HA-29 Backend : 생성자, 수정자 공통 엔티티 설계 및 컴포넌트 생성

--- src/main/java/com/jeonggolee/helpanimal/common/eneity/BaseTimeEntity.java
+++ src/main/java/com/jeonggolee/helpanimal/common/eneity/BaseTimeEntity.java
@@ -2,7 +2,9 @@ package com.jeonggolee.helpanimal.common.eneity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -23,6 +25,13 @@ public class BaseTimeEntity {
     private LocalDateTime updateDate;
 
     private LocalDateTime deletedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updateBy;
 
     public void delete(){
         this.deletedAt = LocalDateTime.now();
