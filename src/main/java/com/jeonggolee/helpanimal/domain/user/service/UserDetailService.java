703b261186c0f01c0c6bfe277202e572a9e63cd6
rhtld123 <rhtld123@gmail.com>
2022-09-12 17:36:34 +0900

    HA-54 Backend : UserDetailService CustomUㄴerDetails로 Return 하도록 변경

--- src/main/java/com/jeonggolee/helpanimal/domain/user/service/UserDetailService.java
+++ src/main/java/com/jeonggolee/helpanimal/domain/user/service/UserDetailService.java
@@ -1,18 +1,15 @@
 package com.jeonggolee.helpanimal.domain.user.service;
 
+import com.jeonggolee.helpanimal.domain.user.dto.CustomUserDetails;
 import com.jeonggolee.helpanimal.domain.user.entity.UserEntity;
 import com.jeonggolee.helpanimal.domain.user.query.UserSpecification;
 import com.jeonggolee.helpanimal.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -24,11 +21,11 @@ public class UserDetailService implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UserEntity userEntity = userRepository.findOne(userSpecification.searchWithEmailEqual(email))
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        UserEntity userEntity = userRepository.findOne(
+                userSpecification.searchWithEmailEqual(email))
+            .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
-        return new User(userEntity.getEmail(), userEntity.getPassword(), Collections.singleton(new SimpleGrantedAuthority(
-            userEntity.getRole().getKey())));
+        return new CustomUserDetails(userEntity);
     }
 
 }
